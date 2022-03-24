4b726770654e64bcf857184a33ccae23553422ff
Stephan Randle <stephanrandle@hotmail.com>
2022-03-24 11:42:49 -0400

    updated routes

--- api/state.py
+++ api/state.py
@@ -1,13 +1,34 @@
-import fastapi
+from optparse import Option
+from typing import Optional
+from fastapi import APIRouter, Path, Query
 
-router = fastapi.APIRouter()
+router = APIRouter()
 
 
-@router.get('/state')
-async def get_state():
-    pass
+@router.get('/state/{state_id}')
+async def get_state_info(
+    state_id: int = Path(
+        ..., description="")
+):
+    """
+    Get individual state tax object
+    """
+    ...
 
 
 @router.get('/states')
-async def get_states():
-    pass
+async def get_states_info(
+    start: Optional[int] = Query(
+        None, description="Starting position for states request"),
+    end: Optional[int] = Query(
+        None, description="Ending position for states request"),
+    year: Optional[int] = Query(
+        2022, description="Year for requested state tax information")
+):
+    """
+    Get all states based on requested information
+    """
+    return {
+        "year": year,
+        "results": end - start if end else 0
+    }
--- api/year.py
+++ api/year.py
@@ -1,8 +1,12 @@
-import fastapi
+from fastapi import APIRouter, Path
 
-router = fastapi.APIRouter()
+from database.schemas.year import Year
+
+router = APIRouter()
 
 
-@router.get('/year')
-async def get_year():
+@router.get('/year/{year_id}', response_model=Year)
+async def get_year(
+    year_id: int = Path(..., description="")
+):
     pass
